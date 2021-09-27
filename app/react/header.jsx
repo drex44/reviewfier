69773f397621627ea9b45647f2dd95b7ff643afd
dhanraj acharya <dhanraj@pirimidtech.com>
2021-09-27 23:55:41 +0530

    Fixed the class name warning in header.jsx

--- app/react/header.jsx
+++ app/react/header.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export const Header = () => (
-  <section class="section navbar-section">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <p class="navbar-item subtitle is-1 homeBtn">
+  <section className="section navbar-section">
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <p className="navbar-item subtitle is-1 homeBtn">
         <strong>Reviewfier</strong>
       </p>
-      <p class="navbar-item subtitle is-2">
-        <span class="subtitle is-4">Find reviews for any product!</span>
+      <p className="navbar-item subtitle is-2">
+        <span className="subtitle is-4">Find reviews for any product!</span>
       </p>
     </nav>
   </section>
