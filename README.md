5ee03571263f1b0b706e5a57f4f6b29312824aa4
dhanraj acharya <dhanraj@pirimidtech.com>
2021-09-28 00:12:31 +0530

    converted product list on home page to react component

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Sample jquery webpack bulma SPA
 
 https://reviewfier.vercel.app
+
+
+## Todo
+- Should have added avg stars in the product lists from the beginning.
+- Add back button in the product page as clicking on the website name to go to home is not apparent.
+- Should have added the product description in the page also.
+- Should have added pagination from the beginning.
--- app/react/header.jsx
+++ app/react/header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { goToHomePage } from "../js/router";
 
 export const Header = () => (
   <section className="section navbar-section">
     <nav className="navbar" role="navigation" aria-label="main navigation">
-      <p className="navbar-item subtitle is-1 homeBtn">
+      <p className="navbar-item subtitle is-1 homeBtn" onClick={goToHomePage}>
         <strong>Reviewfier</strong>
       </p>
       <p className="navbar-item subtitle is-2">
